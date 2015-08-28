3ad8a2f6671b14b3001dd065b90f07e36b6ab51e
ralic <raliclo@gmail.com>
2015-08-27 17:45:41 -0700

    ok

--- CS480-Lab1-MPGCalaculation/src/cs457/hw/CS457HW.java
+++ CS480-Lab1-MPGCalaculation/src/cs457/hw/CS457HW.java
@@ -16,24 +16,23 @@ public class CS457HW {
     /**
      * @param args the command line arguments
      */
-   public static void main(String[] args) {
+    public static void main(String[] args) {
         // TODO code application logic here
-        
+
         Scanner in = new Scanner(System.in);
         System.out.println("Miles Per Gallon Program");
-        double iM,fM,gas;
+        double iM, fM, gas;
         double MPG;
-        
+
         System.out.println("Enter Initial miles:");
         iM = in.nextDouble();
         System.out.println("Enter Final miles:");
         fM = in.nextDouble();
         System.out.println("Enter Gas usage(gallon)");
         gas = in.nextDouble();
-        
-        MPG = (fM-iM)/gas;
-        System.out.printf("Miles per gallon: %.1f\n",MPG);
-        
-    } }
-    
+
+        MPG = (fM - iM) / gas;
+        System.out.printf("Miles per gallon: %.1f\n", MPG);
+
+    }
 }

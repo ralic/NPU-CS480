d33019bc307f9ac6a83ed340bfb9bc20077c070d
ralic <raliclo@gmail.com>
2015-09-22 16:00:43 -0700

    Updates

--- CS480-Lab7-Weishun-FileReader_Writer/src/cs480/lab7/pkg13108/weishun/FRWR.java
+++ CS480-Lab7-Weishun-FileReader_Writer/src/cs480/lab7/pkg13108/weishun/FRWR.java
@@ -17,110 +17,107 @@ import java.io.IOException;
  * @author raliclo
  */
 public class FRWR {
-        private String fileR = "./SampleCSVFile.txt";
-        private String fileW = "./SampleCSVFile-Writer.txt";
-        private FileReader fr;
-        private FileWriter fw;
-        private BufferedReader br;
-        private BufferedWriter bw;
-        private int countLines;
-    
+
+    private String fileR = "./SampleCSVFile.txt";
+    private String fileW = "./SampleCSVFile-Writer.txt";
+    private FileReader fr;
+    private FileWriter fw;
+    private BufferedReader br;
+    private BufferedWriter bw;
+    private int countLines;
+
     /**
      *
      * @throws IOException
      */
-    public FRWR() throws IOException{
-        countLines=this.FRWRexe(this.fileR,this.fileW);
+    public FRWR() throws IOException {
+        countLines = this.FRWRexe(this.fileR, this.fileW);
     }
-       
+
     //FRWRstart help setup FR/BR FW/BR all at a time
-    public void FRWRstart(String filename,String filename2) throws FileNotFoundException, IOException{
+    public void FRWRstart(String filename, String filename2) throws FileNotFoundException, IOException {
         fr = new FileReader(filename);
         br = new BufferedReader(fr);
         fw = new FileWriter(filename2);
         bw = new BufferedWriter(fw);
     }
-    
+
     // FRWRnf generates nth string in each line into another file.
     // spliter = [,] as default
-    public void FRWRnf(int n,String filename2) throws IOException,NullPointerException
-    {
-        FRWRstart(fileR,filename2);
-        
-        String line=br.readLine();
-        int i,j=0;
-        int size=999;
-        int size2=999;
-        int counts=0;
-        
-        String[] splits= line.split("[,]");
-        String[][] splitss = new String[size][size2] ;
-        for (j=0;(line!=null);j++)
-        {
-            for (i=0;i<splits.length;i++)
-            {  
-               //System.out.println(split[i]);
-               splitss[counts][i]=splits[i];
-               System.out.println(splitss[counts][i]);
-               splits= line.split("[,]");
-               if (i==n) {
-                   bw.write(splitss[counts][n]+"\n");
+    // Assumption : each line may have multple [,]
+    // Split : Save 1st line in mutil elements array with indexsize = size.
+    // Splitss : Save next line in multi elements array with 2nd dimension = size2.
+    public void FRWRnf(int n, String filename2) throws IOException, NullPointerException {
+        FRWRstart(fileR, filename2);
+
+        String line = br.readLine();
+        int i, j = 0;
+        int size = 999;
+        int size2 = 999;
+        int counts = 0; // counts save lines.
+
+        String[] splits = line.split("[,]");
+        String[][] splitss = new String[size][size2];
+        for (j = 0; (line != null); j++) {
+            for (i = 0; i < splits.length; i++) {
+                //System.out.println(split[i]);
+                splitss[counts][i] = splits[i];
+                System.out.println(splitss[counts][i]);
+                splits = line.split("[,]");
+                if (i == n) {
+                    bw.write(splitss[counts][n] + "\n");
                 }
-               }
-            line=br.readLine();
+            }
+            line = br.readLine();
             counts++;
         }
         closeall();
     }
-            
-    // FRWRexe returns lines of the file 
+
+    // FRWRexe returns lines of the file
     // first input filename is the source file
     // second input filename is the destination file
-    public int FRWRexe (String filename,String filename2) throws FileNotFoundException,IOException {
-        FRWRstart(filename,filename2);        
-        int count=0;
-        String line=br.readLine();
-        for (int i =1 ; (line != null);i++)
-        {
-        //process each line in some way
-//        System.out.println(i+" Line :"+line);     // skipable    
-        //fw.write(i+" Line :"+line+"\n");
-        bw.append(i+" Line :"+line+"\n");
-        line=br.readLine();        
-        count++;
+    public int FRWRexe(String filename, String filename2) throws FileNotFoundException, IOException {
+        FRWRstart(filename, filename2);
+        int count = 0;
+        String line = br.readLine();
+        for (int i = 1; (line != null); i++) {
+            //process each line in some way
+//        System.out.println(i+" Line :"+line);     // skipable
+            //fw.write(i+" Line :"+line+"\n");
+            bw.append(i + " Line :" + line + "\n");
+            line = br.readLine();
+            count++;
         }
         this.closeall();
         return count;
-        }
-    
+    }
+
     //setter for BR
-    public void setfrbr(String filename) throws FileNotFoundException
-    {
-        this.fr=new FileReader(filename);
-        this.br=new BufferedReader(br);
+    public void setfrbr(String filename) throws FileNotFoundException {
+        this.fr = new FileReader(filename);
+        this.br = new BufferedReader(br);
     }
-    
+
     //setter for BW
-    public void setfwbw(String filename) throws FileNotFoundException, IOException
-    {
-        this.fw=new FileWriter(filename);
-        this.bw=new BufferedWriter(fw);
+    public void setfwbw(String filename) throws FileNotFoundException, IOException {
+        this.fw = new FileWriter(filename);
+        this.bw = new BufferedWriter(fw);
     }
-    
+
     //closeall() help to close every FR/BR BR/BW
-    public void closeall() throws IOException
-    {
+    public void closeall() throws IOException {
         this.br.close();
         this.bw.close();
         this.fr.close();
         this.fw.close();
     }
-    public String getfileR()
-    {
+
+    public String getfileR() {
         return fileR;
     }
-    public String getfileW()
-    {
+
+    public String getfileW() {
         return fileW;
     }
 }
